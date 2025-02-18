cddb9ccc5c883c1d28bd954d390a9092ac4cb2ef
AndresRodriguezT <andresrodrigueztrapero@gmail.com>
2025-02-18 18:53:04 +0100

    try: fix redirection

--- src/app/components/SignupForm.tsx
+++ src/app/components/SignupForm.tsx
@@ -28,7 +28,7 @@ export default function SignUpForm() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       },
     });
     if (error) setError(error.message);
@@ -38,7 +38,7 @@ export default function SignUpForm() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'spotify',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       },
     });
     if (error) setError(error.message);
